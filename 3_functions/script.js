1284654c4d294a8a43d411bc3329f886e69b6810
gromandre <gromov7397@yandex.ru>
2020-11-25 18:49:28 +0400

    last

--- 3_functions/script.js
+++ 3_functions/script.js
@@ -1,6 +1,5 @@
-let userInput;
-let random;
-
+numberFrom = 0;
+numberTo = 1000;
 
 function isNumeric(userNumber) {
     return !isNaN(parseFloat(userNumber)) && isFinite(userNumber);
@@ -8,52 +7,40 @@ function isNumeric(userNumber) {
 
 function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; 
-  }
-  
-random = getRandomIntInclusive(0, 1000)
-console.log(random);
+};
 
 function getUserNumber(){
-    userInput = prompt('Угадайте число');
-
+    let userInput = prompt("Угадайте число от " + numberFrom +  " до " + numberTo);
     while(true){
-        if(userInput==null){
-            return userInput;
+        if(userInput===null){
+            break;
         }
         else if (!isNumeric(userInput)){
-            alert('Ошибка, введите число!');
+            alert("Ошибка, введите число!");
             getUserNumber();
         }
         else{
             break;
         }
-    }
+    }  
+    return userInput;
 }
 
-function compareNumbers(){
-    if(userInput==null){
-        return gameStatus = false;
-    } 
-    if(userInput>random){
-        alert("Меньше, попробуйте еще раз");
-    }
-    if(userInput<random){
-        alert("Больше, попробуйте еще раз");
-    }
-    if(userInput==random){
-        alert("Поздравляю, Вы угадали");
-        return gameStatus = false;
-    }
-}
-let gameStatus = true;
-   while(gameStatus == true){
-    getUserNumber();
-    compareNumbers();
-   }
+let randomNumber = getRandomIntInclusive(numberFrom, numberTo);
     
-
-
-
-
-
-
+while(true){
+    let input = getUserNumber();
+    if(input===null){
+        break;
+    }
+    else if(input>randomNumber){
+        alert("Число меньше, попробуйте еще раз");      
+    }
+    else if(input<randomNumber){
+        alert("Число больше, попробуйте еще раз");  
+    }
+    else{
+        alert("Вы угадали!"); 
+        break;
+    } 
+}
