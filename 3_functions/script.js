390cd31c56473b794a3a9c4cb06418564f129629
gromandre <gromov7397@yandex.ru>
2020-11-26 01:47:09 +0400

    3 попытка

--- 3_functions/script.js
+++ 3_functions/script.js
@@ -1,6 +1,3 @@
-numberFrom = 0;
-numberTo = 1000;
-
 function isNumeric(userNumber) {
     return !isNaN(parseFloat(userNumber)) && isFinite(userNumber);
 };
@@ -10,37 +7,47 @@ function getRandomIntInclusive(min, max) {
 };
 
 function getUserNumber(){
-    let userInput = prompt("Угадайте число от " + numberFrom +  " до " + numberTo);
+    
+    let userInput = prompt('Угадайте число');
+    
     while(true){
         if(userInput===null){
             break;
         }
+        
         else if (!isNumeric(userInput)){
-            alert("Ошибка, введите число!");
+            alert('Ошибка, введите число!');
             getUserNumber();
         }
         else{
+            userInput = parseInt(userInput);
             break;
         }
     }  
     return userInput;
 }
 
-let randomNumber = getRandomIntInclusive(numberFrom, numberTo);
+function startGame(){
+    let numberFrom = 0;
+    let numberTo = 2;
+    let randomNumber = getRandomIntInclusive(numberFrom, numberTo);
     
-while(true){
-    let input = getUserNumber();
-    if(input===null){
-        break;
+    while(true){
+        let input = getUserNumber();
+        console.log(isNaN(input));
+        if(input===null){
+            break;
+        }
+        if(input>randomNumber){
+            alert('Число меньше, попробуйте еще раз');      
+        }
+        if(input<randomNumber){
+            alert('Число больше, попробуйте еще раз');  
+        }
+        if(input==randomNumber){
+            alert('Вы угадали!'); 
+            break;
+        } 
     }
-    else if(input>randomNumber){
-        alert("Число меньше, попробуйте еще раз");      
-    }
-    else if(input<randomNumber){
-        alert("Число больше, попробуйте еще раз");  
-    }
-    else{
-        alert("Вы угадали!"); 
-        break;
-    } 
 }
+startGame();
