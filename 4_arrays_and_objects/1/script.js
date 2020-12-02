54969d152f409e550fcf803f9e47042df93b6a67
gromandre <gromov7397@yandex.ru>
2020-12-02 15:40:31 +0400

    2 вариант

--- 4_arrays_and_objects/1/script.js
+++ 4_arrays_and_objects/1/script.js
@@ -1,30 +1,38 @@
+const delay = 1000; 
+
+setInterval(nowDate, delay);
+
 function nowDate(){
-    let day = new Date(),
+    const day = new Date(),
     weekday = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
     month = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'],
-    declinationHours = ['час', 'часа', 'часов'],
-    declinationMinutes = ['минута', 'минуты', 'минут'],
-    declinationSeconds = ['секунда', 'секунды', 'секунд'];
+    
+    days = day.getDate();
+    months = month[day.getMonth()];
+    years = day.getFullYear();
+    weekdays = weekday[day.getDay()];
+    hours = day.getHours();
+    minutes = day.getMinutes();
+    seconds = day.getSeconds();
+
+    declinationHours = declOfNum(hours,['час', 'часа', 'часов']) ,
+    declinationMinutes = declOfNum(minutes,['минута', 'минуты', 'минут']) ,
+    declinationSeconds = declOfNum(seconds,['секунда', 'секунды', 'секунд']);
    
-    function declOfNum(value, words){  
-        value = Math.abs(value) % 100; 
-        let num = value % 10;
-        if(value > 10 && value < 20) return words[2]; 
-        if(num > 1 && num < 5) return words[1];
-        if(num === 1) return words[0]; 
-        return words[2];
-    }
+    function declOfNum(number, titles) {
+        cases = [2, 0, 1, 1, 1, 2];
+        return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ]; 
+      }
    
-    console.log(
-        'Сегодня ' +day.getDate()+' '+month[day.getMonth()]+' '+day.getFullYear()+' года, '
-        +weekday[day.getDay()]+', '
-        +day.getHours()+' '+declOfNum(day.getHours(),declinationHours)+' '
-        +day.getMinutes()+' '+declOfNum(day.getMinutes(),declinationMinutes)+' '
-        +day.getSeconds()+' '+declOfNum(day.getSeconds(),declinationSeconds)
+    console.log('Сегодня ' 
+        +days+' '+months+' '+years+' года, '
+        +weekdays+', '
+        +hours+' '+declinationHours+' '
+        +minutes+' '+declinationMinutes+' '
+        +seconds+' '+declinationSeconds
     )
 }
-   
-setInterval(nowDate, 1000);
+
    
 
     
--- 4_arrays_and_objects/2/script.js
+++ 4_arrays_and_objects/2/script.js
@@ -1,5 +1,7 @@
+const inputLogin = prompt('Введите логин');
+const inputPassword = prompt('Введите пароль');
+
 const users = [
-    
     {
         name:'Иван',
         login:'ivan',
@@ -12,18 +14,21 @@ const users = [
     }
 ];
 
-const inputLogin = prompt("Введите логин");
-const inputPassword = prompt("Введите пароль");
-let userName;
-   
-for(let i=0;users.length;i++){
-    if((inputLogin===users[i].login) && (inputPassword===users[i].password)){
-        userName = users[i].name;
-        alert('Добрый день, '+userName+'!');
-        break;
-    }
-    if(i===users.length-1){
-        alert('Ошибка авторизации');
-        break;
-    }
-};
+authorization(inputLogin, inputPassword, users);
+
+function authorization(login, password, array){
+    let userName;
+    
+    for(let i=0;users.length;i++){
+        if(login===null || password===null){
+            break;
+        }
+        if((login===array[i].login) && (password===array[i].password)){
+            userName = array[i].name;
+            return alert('Добрый день, '+userName+'!');  
+        }
+        if(i===array.length-1){
+            return alert('Ошибка авторизации');
+        }
+    };
+}
