360a2e21bfa584ce2b52534c8fb15eef64eaca93
gromandre <gromov7397@yandex.ru>
2020-12-03 18:00:54 +0400

    3 вариант

--- 4_arrays_and_objects/1/script.js
+++ 4_arrays_and_objects/1/script.js
@@ -1,37 +1,34 @@
-const delay = 1000; 
+const DELAY_MILISECONDS = 1000; 
 
-setInterval(nowDate, delay);
+setInterval(nowDate, DELAY_MILISECONDS);
+
+function declOfNum(number, titles) {
+    cases = [2, 0, 1, 1, 1, 2];
+    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ]; 
+  };
 
 function nowDate(){
     const day = new Date(),
-    weekday = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-    month = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'],
-    
-    days = day.getDate();
-    months = month[day.getMonth()];
-    years = day.getFullYear();
-    weekdays = weekday[day.getDay()];
-    hours = day.getHours();
-    minutes = day.getMinutes();
-    seconds = day.getSeconds();
+     
+    date = day.toLocaleString('ru-Ru', {day: 'numeric', month: 'long'}),
+    weekdays = day.toLocaleString('ru-Ru', { weekday: 'long' }),
+    years = day.toLocaleString('ru-Ru', {year: 'numeric'}),
+	hours = day.toLocaleString('ru-Ru', {hour: '2-digit'}),
+	minutes = day.toLocaleString('ru-Ru', {minute: '2-digit'}),
+	seconds = day.toLocaleString('ru-Ru', {second: '2-digit'}),
 
-    declinationHours = declOfNum(hours,['час', 'часа', 'часов']) ,
-    declinationMinutes = declOfNum(minutes,['минута', 'минуты', 'минут']) ,
-    declinationSeconds = declOfNum(seconds,['секунда', 'секунды', 'секунд']);
-   
-    function declOfNum(number, titles) {
-        cases = [2, 0, 1, 1, 1, 2];
-        return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ]; 
-      }
+    DECLINATION_HOURS = declOfNum(hours,['час', 'часа', 'часов']) ,
+    DECLINATION_MINUTES = declOfNum(minutes,['минута', 'минуты', 'минут']) ,
+    DECLINATION_SECONDS = declOfNum(seconds,['секунда', 'секунды', 'секунд']);
    
     console.log('Сегодня ' 
-        +days+' '+months+' '+years+' года, '
+        +date+' '+years+' года, '
         +weekdays+', '
-        +hours+' '+declinationHours+' '
-        +minutes+' '+declinationMinutes+' '
-        +seconds+' '+declinationSeconds
+        +hours+' '+DECLINATION_HOURS+' '
+        +minutes+' '+DECLINATION_MINUTES+' '
+        +seconds+' '+DECLINATION_SECONDS
     )
-}
+};
 
    
 
