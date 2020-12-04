97e22acfcabd704be14f32a8008cb8325b9c7e3a
gromandre <gromov7397@yandex.ru>
2020-12-04 23:26:37 +0400

    update

--- 4_arrays_and_objects/1/script.js
+++ 4_arrays_and_objects/1/script.js
@@ -3,14 +3,14 @@ const DELAY_MILISECONDS = 1000;
 setInterval(nowDate, DELAY_MILISECONDS);
 
 function declOfNum(number, titles) {
-    cases = [2, 0, 1, 1, 1, 2];
-    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ]; 
+    const CASES = [2, 0, 1, 1, 1, 2];
+    return titles[ (number%100>4 && number%100<20)? 2 : CASES[(number%10<5)?number%10:5] ]; 
   };
 
 function nowDate(){
     const day = new Date(),
     
-    locales = 'ru-Ru',
+    LOCALES = 'ru-Ru',
 
     performanceDay =  {day: 'numeric', month: 'long'},
     performanceWeekday =  {weekday: 'long'},
@@ -19,12 +19,12 @@ function nowDate(){
     performanceMinute =  {minute: '2-digit'},
     performanceSecond =  {second: '2-digit'},
     
-    date = day.toLocaleString(locales, performanceDay),
-    weekdays = day.toLocaleString(locales, performanceWeekday),
-    years = day.toLocaleString(locales, performanceYear),
-	hours = day.toLocaleString(locales, performanceHour),
-	minutes = day.toLocaleString(locales, performanceMinute),
-    seconds = day.toLocaleString(locales, performanceSecond),
+    date = day.toLocaleString(LOCALES, performanceDay),
+    weekdays = day.toLocaleString(LOCALES, performanceWeekday),
+    years = day.toLocaleString(LOCALES, performanceYear),
+	hours = day.toLocaleString(LOCALES, performanceHour),
+	minutes = day.toLocaleString(LOCALES, performanceMinute),
+    seconds = day.toLocaleString(LOCALES, performanceSecond),
     
     wordformHours = ['час', 'часа', 'часов'],
     wordformMinute = ['минута', 'минуты', 'минут'],
